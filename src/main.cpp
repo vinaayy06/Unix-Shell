ea6a4749180a52e81f0342bedd5e6dbd2545312d
vinaayy06 <vinaysehra0606@gmail.com>
2026-05-27 00:49:13 +0530

    library

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main() {
-  
+int main()
+{
+
+  vector<string> built_in;
+
+  built_in.push_back("echo");
+  built_in.push_back("exit");
+  built_in.push_back("type");
+
   cout << unitbuf;
   cerr << unitbuf;
-  while (true) {
+  while (true)
+  {
     cout << "$ ";
     string input;
     getline(cin, input);
-    if(input == "exit"){
+    if (input == "exit")
+    {
       break;
     }
-    else if(input.size()>=5 &&input.substr(0,5) == "echo "){
+    else if (input.substr(0, 5) == "echo ")
+    {
       cout << input.substr(5) << endl;
     }
-    else{
-      cout<<input<<endl;
+    else if (input.substr(0, 5) == "type ")
+    {
+      string target = input.substr(5);
+      if (find(built_in.begin(), built_in.end(), target) != built_in.end())
+      {
+        cout << target << " is a shell builtin" << endl;
+      }
+      else
+      {
+        cout << target << ": not found" << endl;
+      }
+    }else{
+  cout << input << ": command not found" <<endl;
     }
-    
-    cout<<input<<endl;
+
+  
   }
 }
-
